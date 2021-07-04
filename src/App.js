2434c4fad5c705d91dc77cdd030a272b1e844b94
Vaishak Nair <vaishak.web@gmail.com>
2021-07-04 21:51:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ function App() {
   const removeTour = (id) => {
     const newTours = tours.filter((tours) => tours.id !== id);
     setTours(newTours);
-   }
+  };
   const fetchTours = async () => {
     setLoading(true);
     try {
       // const response = await fetch(url);
       // const tours = await response.json();
       const tours = data;
-      console.log(tours);
+      // console.log(tours);
       setLoading(false);
       setTours(tours);
     } catch (error) {
       setLoading(false);
-      console.log(error);
+      // console.log(error);
     }
   };
   useEffect(() => {
@@ -39,19 +39,21 @@ function App() {
     );
   }
 
-  if(tours.length === 0){
-    return(
+  if (tours.length === 0) {
+    return (
       <main>
         <div className="title">
           <h2>No tours available</h2>
-          <button className="btn" onClick={fetchTours}>Refresh</button>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
         </div>
       </main>
-    )
+    );
   }
   return (
     <main>
-      <Tours tours={tours} removeTour={removeTour}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
